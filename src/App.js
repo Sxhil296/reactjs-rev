3cae58a0cd430e27c53558d163ebf9ba71b43224
Sahil <leviack629@gmail.com>
2023-07-13 13:18:07 +0530

    destructuring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import Counter from "./components/Counter";
 import Greet from "./components/Greet";
 import Hello from "./components/Hello";
 import Message from "./components/Message";
@@ -13,14 +14,15 @@ function App() {
       {/* <Props name="Paterick" message="I have to return some video tapes"/> */}
 
 
-      {/* <Greet name="Bruce" heroName="Batman"> <p>I am vengeance</p></Greet>
+      <Greet name="Bruce" heroName="Batman"> <p>I am vengeance</p></Greet>
       <Greet name="Clark" heroName="Superman"/>
-      <Greet name="Peter" heroName="Spiderman"/> */}
+      <Greet name="Peter" heroName="Spiderman"/>
 
 
       {/* <Welcome /> */}
       {/* <Hello /> */}
-      <Message />
+      {/* <Message /> */}
+      {/* <Counter /> */}
     </div>
   );
 }
